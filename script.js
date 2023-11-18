d0671721ba840742e8dc90e5aa0b081672bc45f2
ProjectDG <87559063+ProjectDG@users.noreply.github.com>
2023-11-18 14:19:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+class Bottle{
+    constructor(name/*, glass, rim, liquor = [], liqueur = [], mixers = [], garnish = [], photo, batch, instructions = []*/){
+     this.name = name;
+     /*
+     this.glass = glass;
+     this.rim = rim;
+     this.liquor = liquor;
+     this.liqueur = liqueur;
+     this.mixers = mixers;
+     this.garnish = garnish;
+     this.photo = photo;
+     this.batch = batch;
+     this.instructions = instructions;
+     */
+    };
+   };
+
