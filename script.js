823c7e2382401d3df0f98e4a936c893196ceaaaa
ProjectDG <87559063+ProjectDG@users.noreply.github.com>
2023-11-30 01:10:18 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-class Bottle{
-    constructor(name/*, glass, rim, liquor = [], liqueur = [], mixers = [], garnish = [], photo, batch, instructions = []*/){
+class Item{
+    constructor(name, contents = []/*, rim, liquor = [], liqueur = [], mixers = [], garnish = [], photo, batch, instructions = []*/){
      this.name = name;
+     this.contents = contents;
      /*
-     this.glass = glass;
      this.rim = rim;
      this.liquor = liquor;
      this.liqueur = liqueur;
@@ -15,6 +15,21 @@ class Bottle{
      */
     };
    };
+
+
+
+  let sangriaBatches = new Item("Sangria Batches", ["Red Sangria Batch", "White Sangria Batch", "Rosé Sangria Batch"]);
+  let syrups = new Item("Syrups", ["Grenadine", "Pomegranate", "Watermelon"]);
+  let glassRimmer = new Item("Glass Rimmer", ["Lime Juice", "Salt", "Sugar", "Hibiscus Salt", "Tajin"]);
+
+
+
+  itemList = [
+    sangriaBatches,
+    syrups,
+    glassRimmer
+  ];
+
 
 
 
@@ -30,19 +45,14 @@ class Bottle{
   }
 
 
+
+
+
   // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn1");
-
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "flex";
-}
+var span = document.getElementsByClassName("close")[0]; 
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -55,3 +65,61 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var mainTitle = document.getElementById("mainTitle");
+var selectedSection = document.getElementById("serviceWellPrinter").alt;
+mainTitle.innerText = selectedSection;
+
+
+$(document).ready(function(){
+
+  // When the user clicks the button, open the modal
+  $('body').on('click','.imageNumbers', function(){
+    modal.style.display = "flex";
+
+    // Resets Modal List
+    $("#modalList").empty();
+    
+    console.log(this);
+    
+
+    itemList.map(x => {
+      console.log(x["name"]);
+      if(this.value === x["name"]){
+        // Sets Modal Title
+        let title = document.getElementById("modalTitle");
+        title.innerText = x["name"];
+
+        let modalList = document.getElementById("modalList");
+    
+        //console.log(x["contents"]);
+        if(x.contents !== null){
+          x.contents.map(i => {
+            //console.log(i);
+            let li = document.createElement('li');
+            li.innerText= i;
+            modalList.append(li);
+          })
+        }
+
+      }
+    });
+    
+  });
+
+});
+
+
+
+/*
+if(x.similarBrands !== null){
+  x.similarBrands.map(i => {
+    //console.log(i);
+    let p = document.createElement('p');
+    p.append(i);
+    similarBrandsElement.append(p);
+  })
+} else {
+  $("#similarBrandsTitle").remove();
+  $("#similarBrandsElement").remove();
+}*/
