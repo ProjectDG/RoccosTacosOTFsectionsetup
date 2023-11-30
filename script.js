5de155109472918ba6a40cd9b20f0e2aa3b3316f
ProjectDG <87559063+ProjectDG@users.noreply.github.com>
2023-11-30 10:11:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 class Item{
-    constructor(name, contents = []/*, rim, liquor = [], liqueur = [], mixers = [], garnish = [], photo, batch, instructions = []*/){
+    constructor(id, name, contents = []/*, rim, liquor = [], liqueur = [], mixers = [], garnish = [], photo, batch, instructions = []*/){
+     this.id = id; 
      this.name = name;
      this.contents = contents;
      /*
@@ -18,16 +19,26 @@ class Item{
 
 
 
-  let sangriaBatches = new Item("Sangria Batches", ["Red Sangria Batch", "White Sangria Batch", "Rosé Sangria Batch"]);
-  let syrups = new Item("Syrups", ["Grenadine", "Pomegranate", "Watermelon"]);
-  let glassRimmer = new Item("Glass Rimmer", ["Lime Juice", "Salt", "Sugar", "Hibiscus Salt", "Tajin"]);
+  let sangriaBatches = new Item("SWPSB", "Sangria Batches", ["Red Sangria Batch", "White Sangria Batch", "Rosé Sangria Batch"]);
+  let syrups = new Item("SWPS", "Syrups", ["Grenadine", "Pomegranate", "Watermelon"]);
+  let glassRimmer = new Item("SWPGR", "Glass Rimmer", ["Lime Juice", "Salt", "Sugar", "Hibiscus Salt", "Tajin"]);
+  let sideRimBitters = new Item("SWPSRB", "Side Rim & Bitters", ["Black Salt", "Classic Bitters", "Chocolate Bitters", "Orange Bitters"]);
+  let mixers = new Item("SWPM", "Mixers", ["Ginger Beer", "Ginger Ale", "Pineapple Juice"]);
+  let tonics = new Item("SWPT", "Tonics", ["Classic Tonic", "Tiki Tonic", "Bloody Mary Mix"]);
+  let printerStand = new Item("SWPPS", "Printer Stand", ["Printer Paper", "Printer Ink", "Cocktail Picks", "Chocolate Bar"]);
+  let extraGarnish = new Item("SWPEG", "Extra Garnish", ["Coffee Beans", "Cinnamon Sugar"]);
 
 
 
   itemList = [
     sangriaBatches,
     syrups,
-    glassRimmer
+    glassRimmer,
+    sideRimBitters,
+    mixers,
+    tonics,
+    printerStand,
+    extraGarnish
   ];
 
 
@@ -74,19 +85,16 @@ mainTitle.innerText = selectedSection;
 $(document).ready(function(){
 
   // When the user clicks the button, open the modal
-  $('body').on('click','.imageNumbers', function(){
+  $('body').on('click','.image-numbers', function(){
     modal.style.display = "flex";
 
     // Resets Modal List
     $("#modalList").empty();
     $("#modalTitle").empty();
     
-    console.log(this);
-    
 
     itemList.map(x => {
-      console.log(x["name"]);
-      if(this.value === x["name"]){
+      if(this.name === x["id"]){
         // Sets Modal Title
         let title = document.getElementById("modalTitle");
         title.innerText = x["name"];
@@ -96,7 +104,6 @@ $(document).ready(function(){
         //console.log(x["contents"]);
         if(x.contents !== null){
           x.contents.map(i => {
-            //console.log(i);
             let li = document.createElement('li');
             li.innerText= i;
             modalList.append(li);
